681c33109667bf1243d72d0e40005af39465322b
“BlanaAndrei” <klaudiu_k@yahoo.com>
2025-07-13 21:49:01 +0300

    feat: add GitHub Copilot plugin; update blink and lspconfig

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -58,6 +58,9 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
 
+  -- Add GitHub Copilot
+  'github/copilot.vim',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --

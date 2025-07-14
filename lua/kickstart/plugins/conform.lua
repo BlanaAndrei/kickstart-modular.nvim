6aaad86320b11538f283a97038a20d1cb4aec160
“BlanaAndrei” <klaudiu_k@yahoo.com>
2025-07-14 20:56:03 +0300

    feat: add Go plugin configuration and linters for Go files

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -27,6 +27,7 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'gofumpt' },
         javascript = { 'eslint_d', 'prettierd' },
         typescript = { 'eslint_d', 'prettierd' },
         javascriptreact = { 'eslint_d', 'prettierd' },
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -9,6 +9,7 @@ return {
       -- Configure linters by filetype
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        go = { 'golangcilint' },
         javascript = { 'eslint_d' },
         typescript = { 'eslint_d' },
         javascriptreact = { 'eslint_d' },
